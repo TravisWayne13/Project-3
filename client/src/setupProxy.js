26cf72efd053a10dc0e163ad7a7c71e409cd68eb
Travis Houston <traviswaynehouston@gmail.com>
2020-01-25 13:51:19 -0800

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/client", , "/otherApi"], { target: "http://localhost:3000" })
+    proxy( { target: "http://localhost:3001" })
   );
 };
