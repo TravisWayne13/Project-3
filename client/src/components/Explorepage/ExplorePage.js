4534c39a30cad721ad0b985b8b02cc37652c438f
Travis Houston <traviswaynehouston@gmail.com>
2020-02-09 10:12:48 -0800

    comment modal

--- client/src/components/Explorepage/ExplorePage.js
+++ client/src/components/Explorepage/ExplorePage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import NavBar from '../NavBar'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import deepPurple from '@material-ui/core/colors/deepPurple'
+import TextField from '@material-ui/core/TextField'
 import PollAPI from '../../utils/PollAPI'
 import './Explorepage.css'
 import commentsSvg from '../../images/comments.svg'
@@ -11,7 +15,27 @@ const { getNewestPolls } = PollAPI
 
 const ExplorePageComp = _ => {
 
+  const useStyles = makeStyles(theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 310,
+      },
+    },
+  }))
+  const classes = useStyles()
+  const theme = createMuiTheme({
+    palette: {
+      primary: deepPurple,
+      secondary: {
+        main: '#f44336',
+      },
+    },
+  })
+ 
   const [data, setData] = useState({ polls: [] })
+  const [modal, setModal] = useState(false)
+  const toggle = () => setModal(!modal)
 
   data.showPoll = e => {
     e.target.nextSibling.style.display = 'block'
@@ -31,7 +55,7 @@ const ExplorePageComp = _ => {
       <div>
         <NavBar/>
         {data.polls.map(poll => (
-          <div className="pollCard">
+          <div className="pollCard" key={poll._id}>
             <div className="pollCreated">
               <img className="pollAvatar" alt="User Avatar" src={poll.user.userAvatar ? poll.user.userAvatar : avatar } />
               <h5 className="pollUsername">{poll.user.username}</h5>
@@ -40,7 +64,24 @@ const ExplorePageComp = _ => {
                 }</h6>
             </div>
             <div>
-              <img className="pollCommentsSvg" src={commentsSvg}/>
+              <img className="pollCommentsSvg" onClick={toggle} src={commentsSvg}/>
+              <div>
+                <Modal isOpen={modal} toggle={toggle}>
+                  <ModalHeader toggle={toggle}>
+                    <img className="pollAvatar" alt="User Avatar" src={poll.user.userAvatar ? poll.user.userAvatar : avatar } />
+                  </ModalHeader>
+                  <ModalBody>
+                    <form className={classes.root} noValidate autoComplete="off">
+                    <ThemeProvider theme={theme}>
+                    <TextField id="outlined-basic" label="Comment" variant="outlined" />
+                    </ThemeProvider>
+                    </form>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button className="post" onClick={toggle}>Post</Button>{' '}
+                  </ModalFooter>
+                </Modal>
+               </div>
               <p className="pollCommentsCount">{Object.keys(poll.votes).reduce((sum,key)=>sum+parseFloat(poll.votes[key]||0),0)}</p>
               <img className="pollVotesSvg" src={votesSvg}/>
               <p className="pollVotesCount">{Object.keys(poll.votes).reduce((sum,key)=>sum+parseFloat(poll.votes[key]||0),0)}</p>
@@ -51,9 +92,9 @@ const ExplorePageComp = _ => {
               <div className="poll">
                 <form>
                   {poll.options.map(option => (
-                    <p>
+                    <p key={option}>
                       <input type="radio"  name={poll._id} value={option} />
-                      <label for={option}>{option}</label>
+                      <label htmlFor={option}>{option}</label>
                     </p>
                   ))}
                   <button>Vote Now</button>
