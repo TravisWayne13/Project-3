f114a4c497686c2aaf17763a004d98a9f59ea256
Bert Keller <bertwinkeller@gmail.com>
2020-03-20 15:38:40 -0700

    new build

---
+++ client/build/precache-manifest.70cc737fbddad6f29513d2ce48f1e16c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314542be0098fccd7acdf8981e9c6371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb41aa14aee7e9dabe0",
+    "url": "/static/css/2.3fc4889c.chunk.css"
+  },
+  {
+    "revision": "75ef9852e9a30c6031d6",
+    "url": "/static/css/main.d440eb4f.chunk.css"
+  },
+  {
+    "revision": "edb41aa14aee7e9dabe0",
+    "url": "/static/js/2.606bffbb.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.606bffbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75ef9852e9a30c6031d6",
+    "url": "/static/js/main.0e5aa030.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d41767be3e6fa2e82f6db352efc9325",
+    "url": "/static/media/Avatar.5d41767b.svg"
+  },
+  {
+    "revision": "b464bee9afbdc5b37818f76963ae0973",
+    "url": "/static/media/Close.b464bee9.svg"
+  },
+  {
+    "revision": "9fdca7567daf08a353b8dcb1affcb524",
+    "url": "/static/media/Create.9fdca756.svg"
+  },
+  {
+    "revision": "1e6f5c276a5d431d7ebdb5cca6c9091c",
+    "url": "/static/media/Poll.1e6f5c27.svg"
+  },
+  {
+    "revision": "224439c82783b9fc54d983839b869730",
+    "url": "/static/media/Right-Chevron.224439c8.svg"
+  },
+  {
+    "revision": "0d6f309bd188565054d2d5ac932f5a7a",
+    "url": "/static/media/comments.0d6f309b.svg"
+  },
+  {
+    "revision": "a159688d4f12d939898df12d886bffde",
+    "url": "/static/media/compass.a159688d.svg"
+  },
+  {
+    "revision": "39db00a2928d459dd3071c6842629246",
+    "url": "/static/media/logo.39db00a2.svg"
+  },
+  {
+    "revision": "d05096a76ecbade48c585dc76afefd7b",
+    "url": "/static/media/menu.d05096a7.svg"
+  },
+  {
+    "revision": "f03f822460f1c5366f1e31adde160892",
+    "url": "/static/media/mock.f03f8224.png"
+  },
+  {
+    "revision": "d5316b45d51ccf4731026aebdcf3610f",
+    "url": "/static/media/votes.d5316b45.svg"
+  }
+]);
